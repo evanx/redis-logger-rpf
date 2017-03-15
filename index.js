9e6fc0cf09b523d0d161ec761683398c5920ced2
Evan Summers <evan.summers@gmail.com>
2017-03-15 19:53:55 +0000

    publish

--- index.js
+++ index.js
@@ -20,24 +20,56 @@ const console_log = (level, ...args) => {
     }
 }
 
-const that = {
-};
+const getName = (level, arg) => {
+    if (typeof arg === 'string') { 
+        return arg;
+    }
+    if (typeof arg === 'object') { 
+        if (arg.name) {
+            return arg.name;
+        }
+        const keys = Object.keys(arg);
+        if (keys.length === 1) {
+            return keys[0];
+        }
+    }
+    return level;
+}
+
+const that = {};
 
 module.exports = (config, redis) => {
+    const client = redis.createClient({
+        host: config.redisHost,
+        port: config.redisPort,
+        password: config.redisPassword
+    });
     const timeLimit = (config.loggerLevel === 'debug') ? 60000 : 600000;
     const log = (level, args) => {
-        const name = (typeof args[0] === 'string') ? args[0] : level;
+        const arg = args[0];  
+        const name = getName(arg);
         const data = getSetDefault(that, name, {timestamp: 0, count: 0});
         data.count++;
         if (Date.now() - data.timestamp > timeLimit) {
+            if (typeof arg === 'object' && arg.name === 'DataError' && arg.data) {
+                const jsonKey = `logger:error:${config.redisNamespace}:j`;
+                client.set(jsonKey, JSON.stringify(arg.data), (err, result) => {
+                    if (err) {
+                        console.error(module.filename, 'set', jsonKey, err.message);
+                    }
+                });
+            }
             console_log(level, data.count, ...args);
             data.timestamp = Date.now();
-        } 
+        }
     };
     return ['debug', 'some', 'info', 'warn', 'error']
     .reduce((logger, level) => {
         logger[level] = (...args) => log(level, args);
         return logger;
     }, {
+       end: () => {
+           client.end(true);
+       }
     });
 };
